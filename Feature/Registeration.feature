563cc0c162693b159f760e460b79bdb836b8a2ba
Darshana <thaveeshadarshana@gmail.com>
2022-12-22 10:43:24 +0800

    added API test
    added API test

--- Feature/Registeration.feature
+++ Feature/Registeration.feature
@@ -8,18 +8,18 @@ Feature:  As a visitor, I want to be able to register in the website So that I c
 #	Then Message displayed Login Successfully
 
 
-#Scenario: Successful Login with Valid Credentials
-#	Given User is on Home Page
-#	When User enters valid username and password
-#	| Username   | Password |
-#	| darshana | User@12345 |
-#	And Verify Login Success
-	
-
-Scenario: Successful Login with Valid Credentials and vote
+Scenario: Successful Login with Valid Credentials
 	Given User is on Home Page
 	When User enters valid username and password
 	| Username   | Password |
-	| darshana7 | User@12345 |
-	And Click on Alfa Romeo logo
-	And Vote to car
+	| darshana | User@12345 |
+	And Verify Login Success
+	
+
+#Scenario: Successful Login with Valid Credentials and vote
+#	Given User is on Home Page
+#	When User enters valid username and password
+#	| Username   | Password |
+#	| darshana7 | User@12345 |
+#	And Click on Alfa Romeo logo
+#	And Vote to car
--- src/cucumbertest/BuggyCarsAPI.java
+++ src/cucumbertest/BuggyCarsAPI.java
@@ -6,45 +6,73 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+
 import static io.restassured.RestAssured.*;
 
 public class BuggyCarsAPI {
 
+	@Test
+	public void getDashboardDetails() {
+		// Specify the base URL to the RESTful web service
+		RestAssured.baseURI = "https://k51qryqov3.execute-api.ap-southeast-2.amazonaws.com/prod/dashboard";
+		// Get the RequestSpecification of the request to be sent to the server
+		RequestSpecification httpRequest = RestAssured.given();
+
+		Response response = httpRequest.get("");
+
+		// Get the status code of the request.
+		// If request is successful, status code will be 200
+		int statusCode = response.getStatusCode();
+		ResponseBody body = response.getBody();
+		System.out.println(body.asString());
+		// Assert that correct status code is returned.
+		Assert.assertEquals(statusCode /* actual value */, 200 /* expected value */, "Correct status code returned");
+	}
+
 //	@Test
-//	public void test1() {
+//	public void registerNewUser() {
+//				
 //
-//
-//		Response response = RestAssured.get("https://buggy.justtestit.org/register");
-//		System.out.println(response.statusCode());
-//		System.out.println(response.asString());
-//		System.out.println(response.getBody().asString());
-//		System.out.println(response.statusLine());
-//
-//		int statusCode = response.getStatusCode();
-//		Assert.assertEquals(statusCode, 200);
+//		RestAssured.baseURI = "https://k51qryqov3.execute-api.ap-southeast-2.amazonaws.com/prod";
+//		RequestSpecification request = RestAssured.given();
+//		JSONObject requestParams = new JSONObject();
+//		
+//		requestParams.put("username", "kamal012");
+//		requestParams.put("firstName", "kamal012");
+//		requestParams.put("lastName", "kamal022");
+//		requestParams.put("password", "User@12345");
+//		requestParams.put("confirmPassword", "User@12345");
+//		
+//		
+//		request.body(requestParams.toJSONString());
+//		
+//		
+//		Response response = request.post("/users");
+//		
+//		ResponseBody body = response.getBody();
+//		System.out.println(response.getStatusCode());
+//		System.out.println(body.asString());
 //
 //	}
-	
-	@Test
-	public void test2() {
+//	
+//	@Test
+//	public void voteForCar() {
+//		 // Specify the base URL to the RESTful web service
+//        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+//        // Get the RequestSpecification of the request to be sent to the server
+//        RequestSpecification httpRequest = RestAssured.given();
+//
+//        Response response = httpRequest.get("");
+//
+//        // Get the status code of the request. 
+//        //If request is successful, status code will be 200
+//       int statusCode = response.getStatusCode();
+//
+//        // Assert that correct status code is returned.
+//       Assert.assertEquals(statusCode /*actual value*/, 200 /*expected value*/, 
+//         "Correct status code returned");
+//	}
 
-
-		JSONObject request = new JSONObject();
-		request.put("username", "q");
-		request.put("firstName", "q");
-		request.put("lastName", "q1");
-		request.put("password", "User@12345");
-		request.put("confirmPassword", "User@12345");
-
-		System.out.println(request);
-		System.out.println(request.toString());
-
-		given().
-		body(request.toJSONString()).
-		when().
-		post("https://k51qryqov3.execute-api.ap-southeast-2.amazonaws.com/prod/users").
-		then().statusCode(201);
-
-	}
-	
 }
